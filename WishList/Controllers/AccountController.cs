db3b9bdf50485fa049a848980da45a1fa8899b1c
ramya-jayaram-core201 <ramya15.j@gmail.com>
2021-06-24 19:00:18 +0530

    Create The AccountController

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ namespace WishList.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
+        //Constructor
         public AccountController(UserManager<ApplicationUser> userManager
             , SignInManager<ApplicationUser> signInManager)
         {
